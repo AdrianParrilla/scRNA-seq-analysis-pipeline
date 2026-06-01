61ed0beae5f2103c70c07b9cc0d684e0de52f757
Adrian Parrilla <125877893+AdrianParrilla@users.noreply.github.com>
2026-06-01 20:27:57 +0200

    Change job partition and output log path

--- scRNA.sh
+++ scRNA.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=nf_scRNA
-#SBATCH --partition=franmartinez
+#SBATCH --partition=short
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1 
 #SBATCH --cpus-per-task=1
-#SBATCH --output=/home/adrianparrilla@vhio.org/logs/%x_%j.log
 #SBATCH --mem=4G
 
 set -euo pipefail
