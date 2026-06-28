92aea50a28c5648d66b0d2011b553a7460231038
AdrianParri <parrilladrian@gmail.com>
2026-06-28 19:24:59 +0200

    error fix

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,6 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
-    "sphinxcontrib.bibtex",
     "sphinx_autodoc_typehints",
     "sphinx.ext.mathjax",
     "IPython.sphinxext.ipython_console_highlighting",
